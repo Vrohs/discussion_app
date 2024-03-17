86cfce7cae4024d6b7ec297884d4940e9faf9d39
Vrohs <info.rohsgaming536@gmail.com>
2024-03-17 16:55:43 +0530

    Update dependencies and remove unused files

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+
 const PORT = process.env.PORT || 3000
 
 
-// middlewares
-
-app.use(express.static('public'))
-app.use(express.json())    // to parse JSON bodies
 
 
+
+app.use(express.static('./public'))
+app.use(express.json())   
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req,res) => {
+
+    res.render('content')
+})
 
 
 app.listen(PORT, () => console.log(`running on http://localhost:${PORT}`))
-
 
